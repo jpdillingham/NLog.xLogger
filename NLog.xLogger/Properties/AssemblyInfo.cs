de3ef7c23b7d6b252e83d52661b65cb54293ad4e
JP Dillingham <jpdillingham@gmail.com>
2017-01-08 15:35:22 -0600

    Updated versioning

--- NLog.xLogger/Properties/AssemblyInfo.cs
+++ NLog.xLogger/Properties/AssemblyInfo.cs
@@ -2,9 +2,8 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// General Information about an assembly is controlled through the following set of attributes. Change these attribute values to
+// modify the information associated with an assembly.
 [assembly: AssemblyTitle("NLog.xLogger")]
 [assembly: AssemblyDescription("A C# Class Library that extends NLog.Logger to provide additional functionality for tracing the entry and exit, arbitrary checkpoints, exceptions and stack traces within methods.")]
 [assembly: AssemblyConfiguration("")]
@@ -14,9 +13,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+// Setting ComVisible to false makes the types in this assembly not visible to COM components. If you need to access a type in this
+// assembly from COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
@@ -24,13 +22,8 @@ using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+// Major Version Minor Version Build Number Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.4.24")]
-[assembly: AssemblyFileVersion("0.9.4.24")]
+// You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below: [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.9.5.25")]
+[assembly: AssemblyFileVersion("0.9.5.25")]
