e7811270014fa562d9ecf99180105b8234014814
JP Dillingham <jpdillingham@gmail.com>
2017-04-24 20:32:46 -0500

    Cleanup of AssemblyInfo.cs

--- NLog.xLogger/Properties/AssemblyInfo.cs
+++ NLog.xLogger/Properties/AssemblyInfo.cs
@@ -1,28 +1,14 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following set of attributes. Change these attribute values to
-// modify the information associated with an assembly.
 [assembly: AssemblyTitle("NLog.xLogger")]
 [assembly: AssemblyDescription("A C# Class Library that extends NLog.Logger to provide additional functionality for tracing the entry and exit, arbitrary checkpoints, exceptions and stack traces within methods.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NLog.xLogger")]
-[assembly: AssemblyCopyright("Copyright (c) 2017 JP Dillingham (jp@dillingham.ws)")]
+[assembly: AssemblyCopyright("Copyright (c) 2016-2017 JP Dillingham (jp@dillingham.ws)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible to COM components. If you need to access a type in this
-// assembly from COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a54d735a-f56b-4caf-8f53-e90f0dc8475a")]
-
-// Version information for an assembly consists of the following four values:
-//
-// Major Version Minor Version Build Number Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below: [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
